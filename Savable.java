d5aedab2124f5e707b4682d9c6cb6f97d9b0df28
Vlad <blackboxorange@gmail.com>
2023-06-23 12:59:00 +0400

    revert 2

--- Savable.java
+++
@@ -1,7 +0,0 @@
-
-import java.io.IOException;
-
-public interface Savable {
-    void save(Object object, String filename) throws IOException;
-    Object load(String filename) throws IOException, ClassNotFoundException;
-}
